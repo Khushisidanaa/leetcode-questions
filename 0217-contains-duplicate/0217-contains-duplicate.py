80ed25af20f6675be74c7fcbd36d3fd0c7724004
Khushisidanaa <sidana.khushi@gmail.com>
2024-01-26 11:15:11 -0600

    Time: 424 ms (79.65%), Space: 27.5 MB (70.63%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hash=set()
+        for i in nums: 
+            if i in hash:
+                return True
+            hash.add(i)
